d193a4b96761b745aca602c5a9d2da0822cc60e1
WillDang14 <will.dang1402@gmail.com>
2025-04-01 10:47:32 -0400

    week-03 assignment

---
+++ src/TodoListItem.jsx
@@ -0,0 +1,12 @@
+function TodoListItem({ todo }) {
+  // console.log(todo);
+
+  return (
+    <>
+      <li>{todo.title}</li>
+    </>
+  );
+}
+
+/* ============================================= */
+export default TodoListItem;

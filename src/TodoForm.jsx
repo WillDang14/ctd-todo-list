b159a8b0632dd38740456ce7da1723bd18c69dbf
WillDang14 <will.dang1402@gmail.com>
2025-04-08 18:23:55 -0400

    week-04

--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -1,9 +1,30 @@
-function TodoForm() {
+import { useRef } from 'react';
+
+/* ============================================= */
+function TodoForm({ onAddTodo }) {
+  const todoTitleInput = useRef('');
+
+  function handleAddTodo(event) {
+    event.preventDefault();
+
+    // console.dir(event.target);
+    // console.dir(event.target.title);
+    // console.dir(event.target.title.value);
+
+    const title = event.target.title.value;
+
+    onAddTodo(title);
+
+    event.target.title.value = '';
+
+    todoTitleInput.current.focus();
+  }
+
   return (
-    <form>
+    <form onSubmit={handleAddTodo}>
       <label htmlFor="todoTitle">Todo</label>
 
-      <input type="text" id="todoTitle" />
+      <input type="text" id="todoTitle" name="title" ref={todoTitleInput} />
 
       <button>Add Todo</button>
     </form>
